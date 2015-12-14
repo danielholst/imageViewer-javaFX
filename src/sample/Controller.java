4d78fe5281c09b80f18ee4e43b3d017f631b1d1b
DanielHolst <danielholst2@gmail.com>
2015-12-14 14:28:19 +0100

    Implemented actions for buttons

--- src/sample/Controller.java
+++ src/sample/Controller.java
@@ -1,4 +1,44 @@
 package sample;
 
+import javafx.event.ActionEvent;
+import javafx.fxml.FXML;
+import javafx.scene.image.ImageView;
+
 public class Controller {
+
+    @FXML
+    ImageView imageView;
+
+    @FXML
+    private void handleRotateRight(ActionEvent event) {
+        // Button was clicked, do something...
+        System.out.println("rotate right, rotation = " + imageView.getRotate());
+
+        imageView.setRotate(imageView.getRotate() + 90);
+    }
+
+    @FXML
+    private void handleRotateLeft(ActionEvent event) {
+        // Button was clicked, do something...
+        System.out.println("rotate left, rotation = " + imageView.getRotate());
+        imageView.setRotate(imageView.getRotate() - 90);
+    }
+
+    @FXML
+    private void handleScaleUp(ActionEvent event) {
+        // Button was clicked, do something...
+        System.out.println("scale up, size = " + imageView.getScaleX());
+        imageView.setScaleX(imageView.getScaleX() / 0.75);
+        imageView.setScaleY(imageView.getScaleY() / 0.75);
+
+    }
+
+    @FXML
+    private void handleScaleDown(ActionEvent event) {
+        // Button was clicked, do something...
+        System.out.println("scale down, size = " + imageView.getScaleX());
+        imageView.setScaleX(imageView.getScaleX() * 0.75);
+        imageView.setScaleY(imageView.getScaleY() * 0.75);
+    }
+
 }
