e233e6225b358e618629cd28ca300f4335ca7651
DanielHolst <danielholst2@gmail.com>
2015-12-14 12:15:36 +0100

    Add imageview and some buttons

--- src/sample/Main.java
+++ src/sample/Main.java
@@ -1,9 +1,12 @@
 package sample;
 
 import javafx.application.Application;
+import javafx.fxml.FXML;
 import javafx.fxml.FXMLLoader;
 import javafx.scene.Parent;
 import javafx.scene.Scene;
+import javafx.scene.image.Image;
+import javafx.scene.image.ImageView;
 import javafx.stage.Stage;
 
 public class Main extends Application {
@@ -11,10 +14,17 @@ public class Main extends Application {
     @Override
     public void start(Stage primaryStage) throws Exception{
         Parent root = FXMLLoader.load(getClass().getResource("sample.fxml"));
-        primaryStage.setTitle("Hello World");
-        primaryStage.setScene(new Scene(root, 800, 600));
+        primaryStage.setTitle("Image viewer");
+        primaryStage.setScene(new Scene(root, 1200, 1000));
+
+        //Image image = new Image("@image.jpg"); //, 800, 600, false, true);
+
+        //ImageView imageView = null;
+        //imageView.setImage(image);
+
         primaryStage.show();
     }
+
 
 
     public static void main(String[] args) {
