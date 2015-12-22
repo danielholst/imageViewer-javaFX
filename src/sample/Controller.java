2073ecf69fd3a73240d116b88bec0400ed37b3b1
DanielHolst <danielholst2@gmail.com>
2015-12-22 19:02:42 +0100

    Add comments and clean up

--- src/sample/Controller.java
+++ src/sample/Controller.java
@@ -1,10 +1,14 @@
 package sample;
 
-import javafx.event.ActionEvent;
 import javafx.fxml.FXML;
+
+import javafx.event.ActionEvent;
 import javafx.scene.image.Image;
 import javafx.scene.image.ImageView;
 
+/**
+ * Controller class to handle button actions
+ */
 public class Controller {
 
     @FXML
@@ -13,7 +17,9 @@ public class Controller {
     private static Image image1, image2, image3;
     private static int id;
 
+    /** Initialize array of images */
     public static void init() {
+
         id = 0;
         image1 = new Image("image1.jpg", 800, 600, false, true);
         image2 = new Image("image2.jpg", 800, 400, false, true);
@@ -25,8 +31,9 @@ public class Controller {
     }
 
     @FXML
+    /** Button to show next image */
     private void handleNextImage(ActionEvent event) {
-        // Button was clicked, do something...
+
         System.out.println("load next image, image size = " + image2.getWidth() + ", " + image2.getHeight());
         if(id == images.length -1) {
             imageView.setImage(images[0]);
@@ -39,8 +46,9 @@ public class Controller {
     }
 
     @FXML
+    /** Button to show previous image */
     private void handlePrevImage(ActionEvent event) {
-        // Button was clicked, do something...
+
         System.out.println("load previous image");
         if(id == 0) {
             imageView.setImage(images[images.length-1]);
@@ -53,35 +61,36 @@ public class Controller {
     }
 
     @FXML
+    /** Button to rotate image to the right*/
     private void handleRotateRight(ActionEvent event) {
-        // Button was clicked, do something...
-        System.out.println("rotate right, rotation = " + imageView.getRotate());
 
+        System.out.println("rotate right, rotation = " + imageView.getRotate());
         imageView.setRotate(imageView.getRotate() + 90);
     }
 
     @FXML
+    /** Button to rotate image to the left*/
     private void handleRotateLeft(ActionEvent event) {
-        // Button was clicked, do something...
+
         System.out.println("rotate left, rotation = " + imageView.getRotate());
         imageView.setRotate(imageView.getRotate() - 90);
     }
 
     @FXML
+    /** Button to scale up image */
     private void handleScaleUp(ActionEvent event) {
-        // Button was clicked, do something...
+
         System.out.println("scale up, size = " + imageView.getScaleX());
         imageView.setScaleX(imageView.getScaleX() / 0.75);
         imageView.setScaleY(imageView.getScaleY() / 0.75);
-
     }
 
     @FXML
+    /** Button to scale down image */
     private void handleScaleDown(ActionEvent event) {
-        // Button was clicked, do something...
+
         System.out.println("scale down, size = " + imageView.getScaleX());
         imageView.setScaleX(imageView.getScaleX() * 0.75);
         imageView.setScaleY(imageView.getScaleY() * 0.75);
     }
-
 }
